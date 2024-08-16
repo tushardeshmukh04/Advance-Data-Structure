725e499bc859b436b4657993ace8a129d0e9f1a7
tushardeshmukh04 <tushardeshmukh187@gmail.com>
2024-08-17 02:08:37 +0530

    Maximum Subarrays Sum

---
+++ ArrayInJava/prefixSumOfSubarrays.java
@@ -0,0 +1,28 @@
+package ArrayInJava;
+
+public class prefixSumOfSubarrays {
+    public  static void maxSubarraysSum(int[] arr){
+        int[] prefix = new int[arr.length];
+        int maxSum = Integer.MIN_VALUE;
+        prefix[0] = arr[0];
+        //calculate prefixSum array
+        for(int i=1; i<prefix.length; i++){
+            prefix[i] = prefix[i-1] + arr[i];
+        }
+        // Calculate maximum  sum of subarrays ;
+        for(int i=0; i<arr.length; i++){
+            int currSum = 0;
+            for(int j=0; j<arr.length; j++){
+              currSum = i == 0 ? prefix[j] : prefix[j] - prefix[i-1];
+            }
+            if(maxSum < currSum){
+                maxSum = currSum;
+            }
+        }
+        System.out.println("Maximum sum of subarrays is : " + maxSum);
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, -2, 6, -1, 3};
+        maxSubarraysSum(arr);
+    }
+}
--- ArrayInJava/subarrays.java
+++ ArrayInJava/subarrays.java
@@ -2,18 +2,30 @@ package ArrayInJava;
 
 public class subarrays {
     public static void printSubarrays(int[] arr){
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
         for(int i=0; i<arr.length; i++){
             for(int j=i; j<arr.length; j++){
+                int sum = 0;
                 for(int k=i; k<=j; k++){
+                    sum = sum + arr[k];
                     System.out.print(arr[k]+" ");
+                }
+                if(max < sum){
+                    max = sum;
+                }
+                if(min > sum){
+                    min = sum;
                 }
                 System.out.println();
             }
             System.out.println();
         }
+        System.out.println("Maximum addition of subArrays is:"+ max);
+        System.out.println("Minimum addition of subArrays is:"+ min);
     }
     public static void main(String[] args) {
-        int[] arr = {2,4,6,8,10};
+        int[] arr = {2, 4, 6, 8, 10};
         printSubarrays(arr);
     }
 }
