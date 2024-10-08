f89cb14171c6ed45dfb717fccf9bdce954dc4249
Tushar Deshmukh <tushardeshmukh187@gmail.com>
2024-10-08 17:41:11 +0530

    Bit Manipulation

---
+++ BitManipulation/ClearIthBit.java
@@ -0,0 +1,16 @@
+package BitManipulation;
+
+public class ClearIthBit {
+
+        public static int clearBit(int n, int i) {
+            int bitMask = ~(1<<i);
+            return n & bitMask;
+        }
+
+        public static void main(String[] args) {
+
+            System.out.println(clearBit(10,1));
+        }
+
+
+}
