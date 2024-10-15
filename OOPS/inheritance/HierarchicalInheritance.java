d9e3f0836f2f2889eda0eb039fb38780496466d4
Tushar Deshmukh <tushardeshmukh187@gmail.com>
2024-10-15 18:35:50 +0530

    Inheritance

---
+++ OOPS/inheritance/HierarchicalInheritance.java
@@ -0,0 +1,40 @@
+package OOPS.inheritance;
+
+public class HierarchicalInheritance {
+    public static void main(String[] args) {
+         System.out.println("--------Tesla------");
+         Tesla t1 = new Tesla();
+         t1.autopilot();
+         t1.run();
+         t1.name = "Cars";
+         System.out.println("--------Thar------");
+         Thar t2 = new Thar();
+         t2.offRoadCapability();
+         t2.run();
+         System.out.println("-------RollsRoyce------");
+         RollsRoyce rr = new RollsRoyce();
+         rr.Assist();
+         rr.run();
+    }
+}
+class Car {
+    String name;
+    void run(){
+        System.out.println("can Run");
+    }
+}
+class Tesla extends Car {
+    void autopilot(){
+        System.out.println("AutoPilot");
+    }
+}
+class Thar extends Car {
+    void offRoadCapability() {
+        System.out.println("Off-Road Capability");
+    }
+}
+class RollsRoyce extends Car {
+    void Assist() {
+        System.out.println("A system that connects your car to an emergency services call center");
+    }
+}
