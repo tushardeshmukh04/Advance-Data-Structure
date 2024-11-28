4cd4a6f4de9b3d6587515bca78173087ebb8823c
Tushar Deshmukh <tushardeshmukh187@gmail.com>
2024-11-28 22:41:16 +0530

    Divide And Conquer

---
+++ DivideAndConquer/MergeSort.java
@@ -0,0 +1,58 @@
+package DivideAndConquer;
+
+public class MergeSort {
+    public static void mergeSort(int[] arr, int si, int ei){
+        if(si >= ei){
+            return;
+        }
+        // Work
+        int mid = si + (ei - si) / 2;
+        mergeSort(arr, si, mid);
+        mergeSort(arr, mid + 1, ei);
+        merge(arr, si, mid, ei);
+    }
+    public static void merge(int[] arr, int si, int mid, int ei) {
+        int[] temp = new int[ei - si + 1];
+        int i = si; // iterator for left part
+        int j = mid+1; // iterator for right part
+        int k = 0; // iterator for temp arr
+
+        while(i <= mid && j <= ei){
+            if(arr[i] < arr[j]){
+                temp[k] = arr[i];
+                i++;
+            } else {
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        // left Part
+        while(i <= mid){
+            temp[k++] = arr[i++];
+        }
+
+        // Right Part
+        while(j <= ei){
+            temp[k++] = arr[j++];
+        }
+
+        // copy temp array into original array
+        for(k = 0, i = si; k < temp.length; k++, i++){
+            arr[i] = temp[k];
+        }
+
+    }
+    public static void printArr(int[] arr) {
+        for(int i : arr){
+            System.out.print(i + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {6, 3, 9, 5, 2, 8};
+        mergeSort(arr, 0, arr.length-1);
+        printArr(arr);
+
+    }
+}
---
+++ Recursion/PrintXToThePowerN.java
@@ -0,0 +1,29 @@
+package Recursion;
+
+public class PrintXToThePowerN {
+    public static int printPower(int x, int n) {
+        // time complexity = O(N);
+        if(n == 0){
+            return 1;
+        }
+        return x * printPower(x ,n-1);
+    }
+    public static int optimizedPower(int x, int n) {
+        // time complexity = O(logN);
+        if(n == 0){
+            return 1;
+        }
+        int halfPower = optimizedPower(x, n/2);
+        int halfPowerSq = halfPower * halfPower;
+        if(n % 2 != 0){
+            halfPowerSq *= x;
+        }
+        return halfPowerSq;
+    }
+    public static void main(String[] args) {
+        int x = 2;
+        int n = 5;
+        System.out.println(printPower(x, n));
+        System.out.println(optimizedPower(x, n));
+    }
+}
