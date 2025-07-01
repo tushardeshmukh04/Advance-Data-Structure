505bfbae1318f002a6abe5ae47e49bdc9e8e314b
Tushar Deshmukh <tushardeshmukh187@gmail.com>
2025-07-01 17:22:36 +0530

    Doubly LL Operation

---
+++ LinkedList/DoublyLinkedList.java
@@ -0,0 +1,124 @@
+package LinkedList;
+
+public class DoublyLinkedList {
+    public class Node{
+        int data;
+        Node next;
+        Node prev;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void print(){
+        if(head == null){
+            System.out.println("Doubly LL is Empty");
+        }
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + "<->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    // Add First
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    //Remove First
+    public int removeFirst(){
+        if(head == null){
+            System.out.println("Empty");
+            return Integer.MIN_VALUE;
+        }
+        if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        head.prev = null;
+        size--;
+        return val;
+    }
+
+    //Add Last
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+    }
+
+    //Remove Last
+    public int removeLast(){
+        if(head == null){
+            System.out.println("Empty");
+            return Integer.MIN_VALUE;
+        }
+        if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+        int val = tail.data;
+        tail = tail.prev;
+        tail.next = null;
+        size--;
+        return val;
+    }
+
+    // Reverse Doubly LinkedList
+    public void reverse(){
+        Node curr = head;
+        Node prev = null;
+        Node next;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            curr.prev = next;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+    public static void main(String[] args) {
+        DoublyLinkedList dll = new DoublyLinkedList();
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+        dll.addLast(4);
+        dll.print();
+        System.out.println(size);
+        System.out.println(dll.removeLast());
+        dll.print();
+        System.out.println(size);
+        System.out.println(dll.removeFirst());
+        dll.print();
+        dll.reverse();
+        dll.print();
+    }
+}
