e23b365c6a2d7b25927eebf7bd1f05ffbd532f71
Tushar Deshmukh <tushardeshmukh187@gmail.com>
2025-08-05 01:44:20 +0530

    Build a Binary Search Tree

---
+++ BinarySearchTree/BuildATree.java
@@ -0,0 +1,42 @@
+package BinarySearchTree;
+
+public class BuildATree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+        }
+    }
+    public static Node insert(Node root, int val){
+        if(root == null){
+            root = new Node(val);
+            return root;
+        }
+        if(root.data > val){
+            // Left SubTree
+            root.left = insert(root.left, val);
+        } else {
+            // Right SubTree
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+    public static void inorder(Node root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+    public static void main(String[] args) {
+        int[] values = {5, 1, 3 ,4, 2, 7};
+        Node root = null;
+        for (int value : values) {
+            root = insert(root, value);
+        }
+        inorder(root);
+    }
+}
