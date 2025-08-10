1a9846a5dd6a4e54aee8a8646f80c8aa1d05f70f
Tushar Deshmukh <tushardeshmukh187@gmail.com>
2025-08-10 12:59:08 +0530

    Majority Elements

---
+++ Hashing/HashMap/MajorityElements.java
@@ -0,0 +1,24 @@
+// Given an integer array of size n, find all elements that appear more than [n/3] times
+package Hashing.HashMap;
+
+import java.util.HashMap;
+
+public class MajorityElements {
+    public static void main(String[] args){
+        int[] arr = {1,3,2,5,1,3,1,5,1};
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < arr.length; i++){
+//            if(map.containsKey(arr[i])){
+//                map.put(arr[i], map.get(arr[i])+1);
+//            } else{
+//                map.put(arr[i],1);
+//            }
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        for(Integer key : map.keySet()){
+            if(map.get(key) > arr.length/3){
+                System.out.println(key);
+            }
+        }
+    }
+}
