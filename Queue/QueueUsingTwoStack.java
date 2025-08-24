34065e17b9cb15737fa560d14fae5b68edbab635
Tushar Deshmukh <tushardeshmukh187@gmail.com>
2025-08-24 22:52:53 +0530

    Queue Implementation Using Two Stack

---
+++ Queue/QueueUsingTwoStack.java
@@ -0,0 +1,54 @@
+package Queue;
+
+import java.util.Queue;
+import java.util.Stack;
+
+public class QueueUsingTwoStack {
+    static class Queue {
+        static Stack<Integer> s1 = new Stack<>(); // store elements
+        static Stack<Integer> s2 = new Stack<>(); // used for remove function
+
+        //isEmpty
+        public static boolean isEmpty(){
+            return s1.isEmpty();
+        }
+        //add
+        public static void add(int data){
+            while (!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+            s1.push(data);
+            while (!s2.isEmpty()){
+                s1.push(s2.pop());
+            }
+        }
+        //remove
+        public static int remove(){
+            if(s1.isEmpty()){
+                System.out.println("empty queue");
+                return -1;
+            }
+            return s1.pop();
+        }
+        //peek
+        public static int peek(){
+            if(s1.isEmpty()){
+                System.out.println("empty queue");
+                return -1;
+            }
+            return s1.peek();
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue q = new Queue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        while (!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+
+    }
+}
