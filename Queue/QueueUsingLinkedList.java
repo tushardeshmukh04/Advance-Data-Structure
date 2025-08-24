184c3dc07bdfeb70220ec196b6b0fa924cc7e843
Tushar Deshmukh <tushardeshmukh187@gmail.com>
2025-08-24 22:19:43 +0530

    Queue Implementation Using LinkedList

---
+++ Queue/QueueUsingLinkedList.java
@@ -0,0 +1,65 @@
+package Queue;
+
+public class QueueUsingLinkedList {
+    static class Node {
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    static class Queue {
+        static Node head = null;
+        static Node tail = null;
+
+        public static boolean isEmpty(){
+            return head == null && tail == null;
+        }
+        //add
+        public static void add(int data){
+            Node newNode = new Node(data);
+
+            if(isEmpty()){
+                head = tail = newNode;
+                return;
+            }
+            tail.next = newNode;
+            tail = newNode;
+        }
+        //remove
+        public static int remove(){
+            if(isEmpty()){
+                System.out.println("queue is empty");
+                return -1;
+            }
+            int front = head.data;
+            if(head == tail){
+                head = tail = null;
+            } else {
+                head = head.next;
+            }
+            return front;
+        }
+        //peek
+        public static int peek(){
+            if(isEmpty()){
+                System.out.println("Empty Queue");
+                return -1;
+            }
+            return head.data;
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue q = new Queue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        while (!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+
+    }
+}
