d7c285b4df7d95c88f52cad28981f7a6cd83c17e
Tushar Deshmukh <tushardeshmukh187@gmail.com>
2025-09-03 18:59:05 +0530

    Word Break Problem

---
+++ TrieTree/WordBreakProblem.java
@@ -0,0 +1,66 @@
+package TrieTree;
+
+public class WordBreakProblem {
+    static class Node{
+        Node[] children;
+        boolean eow;
+        public Node(){
+            children = new Node[26];
+            for(int i=0; i<26; i++){
+                children[i] = null;
+            }
+            eow = false;
+        }
+    }
+    static Node root = new Node();
+
+    public static void insert(String word){
+        Node curr = root;
+        for(int i=0; i<word.length(); i++){
+            int idx = word.charAt(i) - 'a';
+            if(curr.children[idx] == null){
+                curr.children[idx] = new Node();
+            }
+            if(i == word.length()-1){
+                curr.children[idx].eow = true;
+            }
+            curr = curr.children[idx];
+        }
+    }
+    public static boolean search(String key){
+        Node curr = root;
+        for(int i=0; i<key.length(); i++){
+            int idx = key.charAt(i) - 'a';
+            if(curr.children[idx] == null){
+                return false;
+            }
+            if(i == key.length()-1 && curr.children[idx].eow == false){
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return true;
+    }
+    public static boolean wordBreak(String key){
+        if(key.length() == 0){
+            return true;
+        }
+        for(int i=1; i<key.length(); i++){
+            String firstPart = key.substring(0, i);
+            String secPart = key.substring(i);
+            if(search(firstPart) && wordBreak(secPart)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"i", "like", "sam", "samsung", "mobile", "ice"};
+        String key = "ilikesamsung";
+        for(int i=0; i<words.length; i++){
+            insert(words[i]);
+        }
+        System.out.println(wordBreak(key));
+    }
+}
