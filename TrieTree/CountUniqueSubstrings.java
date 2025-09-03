3eb0f10d612af7752ca1be9ebc17bb2849651ce8
Tushar Deshmukh <tushardeshmukh187@gmail.com>
2025-09-03 19:34:12 +0530

    Count Unique Sunstring Of A String

---
+++ TrieTree/CountUniqueSubstrings.java
@@ -0,0 +1,50 @@
+package TrieTree;
+
+public class CountUniqueSubstrings {
+    static class Node {
+        Node[] children ;
+        boolean eow;
+        public Node() {
+            children = new Node[26];
+            for(int i=0; i<26; i++){
+                children[i] = null;
+            }
+            eow = false;
+        }
+    }
+    static Node root = new Node();
+    public static void insert(String word){
+        Node curr = root;
+        for(int i=0; i<word.length(); i++){
+            int idx = word.charAt(i) - 'a';
+            if(curr.children[idx] == null){
+                curr.children[idx] = new Node();
+            }
+            if(i == word.length()-1){
+                curr.children[idx].eow = true;
+            }
+            curr = curr.children[idx];
+        }
+    }
+    public static int countNode(Node root){
+        if(root == null){
+            return 0;
+        }
+        int count = 0;
+        for(int i=0; i<26; i++){
+            if(root.children[i] != null){
+                count += countNode(root.children[i]);
+            }
+        }
+        return count+1;
+    }
+    public static void main(String[] args) {
+        String str = "ababa";
+        for(int i=0; i<str.length(); i++){
+            String suffix = str.substring(i);
+            insert(suffix);
+        }
+        System.out.println(countNode(root));
+    }
+
+}
